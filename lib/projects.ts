07abafbab02736bac1278a85cae58d21b03db5bd
Cathleen <c.vanduuren16@gmail.com>
2026-09-14 16:21:32 +0200

    404 lols

--- lib/projects.ts
+++ lib/projects.ts
@@ -9,25 +9,25 @@ export const projects: Project[] = [
   {
     name: 'My website',
     description: 'Bla bla bla bla',
-    link: '#',
+    link: 'bla',
     photo: '/projects/blackkitty.jpeg',
   },
   {
     name: 'Boyfriend website',
     description: 'Bla bla bla bla',
-    link: '#',
+    link: 'bla',
     photo: '/projects/flirtykitty.jpeg',
   },
     {
     name: 'Sister website',
     description: 'Bla bla bla bla',
-    link: '#',
+    link: 'bla',
     photo: '/projects/alienkitty.jpeg',
   },
       {
     name: 'Mom website',
     description: 'Bla bla bla bla',
-    link: '#',
+    link: 'bla',
     photo: '/projects/brownkitty.jpeg',
   },
 ]
